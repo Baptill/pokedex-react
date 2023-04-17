0a889cdf74f961b4ca17ef0742af772b136af69a
Baptiste Lemonnier <baptiste.lmn@gmail.com>
2023-04-17 16:12:24 +0200

    end props 1h58

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
 import React, { FunctionComponent, useEffect, useState} from 'react';
-import Pokemon from './models/pokemon';
-import POKEMONS from './models/mock-pokemon';
+import PokemonList from './pages/pokemon-list';
   
 const App: FunctionComponent = () => {
  
-    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-    
-    useEffect(() => {
-        setPokemons(POKEMONS);
-
-    }, [])
-     
-
     return (
-        <div>
-            <h1>Pokédex</h1>
-            <ul>
-                {pokemons.map(({id, name, picture, created, types}) => (
-                    <div key={name}>
-                        <div>{name}</div>
-                        <div>{id}</div>
-                        <img src={picture} alt={name}/>
-                        <div>{types}</div>
-                    </div>
-                ))}
-            </ul>
-        </div>
+       <PokemonList />
     );
 }
   
